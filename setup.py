631705015d9d896bfde2a2651da9a9f9da7b9a92
Owen Synge <owen.synge@jaysnest.de>
2015-04-15 17:54:48 +0200

    added support for nosetests

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
-
 from VmRecast.__version__ import version
 from sys import version_info
 
+if version_info < (2, 6):
+    import sys
+    print "Please use a newer version of python"
+    sys.exit(1)
+
 
 try:
-	from distutils.core import setup
-except:
+    from setuptools import setup, find_packages
+except ImportError:
 	try:
-        	from setuptools import setup, find_packages
+            from distutils.core import setup
 	except ImportError:
-        	from ez_setup import use_setuptools
-        	use_setuptools()
-        	from setuptools import setup, find_packages
-
+            from ez_setup import use_setuptools
+            use_setuptools()
+            from setuptools import setup, find_packages
+# we want this module for nosetests
+try:
+    import multiprocessing
+except ImportError:
+    # its not critical if this fails though.
+    pass
 
 setup(name='vmrecaster',
     version=version,
@@ -21,11 +30,10 @@ setup(name='vmrecaster',
     author="O M Synge",
     author_email="owen.synge@desy.de",
     license='Private',
-    
-    url = 'none',
     packages = ['VmRecast'],
+    url = 'https://github.com/hepix-virtualisation/vmcatcher',
     classifiers=[
-        'Development Status :: 1 - UnStable',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research'
@@ -34,7 +42,20 @@ setup(name='vmrecaster',
         'Operating System :: POSIX',
         'Programming Language :: Python',
         ],
-
+    install_requires=[
+       "vmcatcher",
+        ],
     scripts=['vmrecaster'],
-    data_files=[('/usr/share/doc/vmrecaster-%s' % (version),['README','ChangeLog','LICENSE'])]
+    data_files=[('/usr/share/doc/vmrecaster-%s' % (version),['README','ChangeLog','LICENSE'])],
+    tests_require=[
+        'coverage >= 3.0',
+        'nose >= 1.1.0',
+        'mock',
+        'SQLAlchemy >= 0.7.8',
+    ],
+    setup_requires=[
+        'nose',
+        'SQLAlchemy >= 0.7.8',
+    ],
+    test_suite = 'nose.collector',
     )
